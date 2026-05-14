84cd86b3c6ba48b65d9d65074afd45391f5ca66d
Shelby Nunn <shelbynunn148@gmail.com>
2026-05-14 14:56:56 -0500

    Added jump script and inventory

--- Assets/Scripts/BadCatController.cs
+++ Assets/Scripts/BadCatController.cs
@@ -9,11 +9,20 @@ public class BadCatController : MonoBehaviour
     public float horizontalInput;
 
     // Controls player speed
-    public float speed = 2.0f;
+    public float moveSpeed = 2.0f;
+
+    // Controls player jump force
+    public float jumpForce = 1.0f;
+
+    // References Rigidbody
+    private Rigidbody rb;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        bool itemCarry = true;
+        bool itemHave = true;
         
     }
 
@@ -25,9 +34,22 @@ public class BadCatController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
 
         // Moves the player forwards and backwards
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * forwardInput);
 
         // Moves the player left and right
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput );
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * horizontalInput );
+
+        // Allows player to jump
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            transform.position += new Vector3(0, jumpForce / 5, 0);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        // Allows player to pick up an object
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+
+        }
     }
 }
