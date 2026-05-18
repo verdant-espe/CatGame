effd81dcd71c747cc1d9c1e183dccd2cc4551f24
Shelby Nunn <shelbynunn148@gmail.com>
2026-05-18 15:14:02 -0500

    Trying to add water physics

---
+++ Assets/Scripts/SwimInWater.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SwimInWater : MonoBehaviour
+{
+    // References player
+    public GameObject Player;
+
+    // Sets gravity for when the player is in water
+    public float waterGrav = 3;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // If player gets in water, gravity changes
+}
