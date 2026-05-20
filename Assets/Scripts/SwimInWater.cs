d050de9a4330cea74c26cf7532a7987786d1f30e
Shelby Nunn <shelbynunn148@gmail.com>
2026-05-20 14:16:24 -0500

    Water Physics Attempt 1

--- Assets/Scripts/SwimInWater.cs
+++ Assets/Scripts/SwimInWater.cs
@@ -5,8 +5,11 @@ public class SwimInWater : MonoBehaviour
     // References player
     public GameObject Player;
 
+    // References Water
+    public GameObject Water;
+
     // Sets gravity for when the player is in water
-    public float waterGrav = 3;
+    public float waterGrav = 0.9f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,4 +24,11 @@ public class SwimInWater : MonoBehaviour
         
     }
     // If player gets in water, gravity changes
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Player)
+        {
+            Physics.gravity = Vector3.down * waterGrav;
+        }
+    }
 }
