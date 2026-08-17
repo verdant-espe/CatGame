51c767c72d5d6d5795351e5f7b8030d7e53aba2a
Shelby Nunn <shelbynunn148@gmail.com>
2026-08-17 15:14:59 -0500

    Working on LoadCustomScene

--- Assets/Scripts/LoadCustomScene.cs
+++ Assets/Scripts/LoadCustomScene.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadCustomScene : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Loads in the Grassland Terrain
+   public void OnStartCLick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Grassland");
     }
 }
