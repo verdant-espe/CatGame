370adbe4c798fc37ff631d2aba0e4620a960de29
Shelby Nunn <shelbynunn148@gmail.com>
2026-09-08 15:06:05 -0500

    Fixing placement system, try to make remove system

---
+++ Assets/Scripts/InteractablePlacer.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablePlacer : MonoBehaviour
+{
+    // Creates a list for GameObjects
+    private List<GameObject> placedGameObject = new();
+}
--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 public class PlacementSystem : MonoBehaviour
 {
     // Used to detect which position on the ground you're selecting using the mouse pointer
-    [SerializeField] GameObject mouseIndicator;
+    [SerializeField] GameObject mouseIndicator, cellIndicator;
 
     // Creates a private property for InputManager
     [SerializeField] private InputManager inputManager;
@@ -26,14 +26,17 @@ public class PlacementSystem : MonoBehaviour
     // Creates data for terrain ground and blocks
     private GridData groundData, blockData;
 
+    // Creates a preview of the item
+    private Renderer previewRenderer;
+
     // Creates a list for GameObjects
     private List<GameObject> placedGameObject = new();
 
-    // References PreviewSystem
-    [SerializeField] private PreviewSystem preview;
-
     // Creates placement preview
     private Vector3Int lastDetectedPosition = Vector3Int.zero;
+
+    // References InteractablePlacer
+    [SerializeField] private InteractablePlacer interactablePlacer;
 
     private void Start()
     {
@@ -43,6 +46,9 @@ public class PlacementSystem : MonoBehaviour
 
         // Defines blockData
         blockData = new();
+
+        // Accesses preview of item
+        previewRenderer = cellIndicator.GetComponentInChildren<Renderer>();
     }
 
     public void StartPlacement(int ID)
@@ -59,8 +65,8 @@ public class PlacementSystem : MonoBehaviour
         // Called when interactable has ID, can be placed
         gridVisual.SetActive(true);
 
-        // Accesses prefab and size of preview item
-        preview.ShowPlacementPreview(database.interactableData[selectedInteractableIndex].Prefab, database.interactableData[selectedInteractableIndex].Size);
+        // Shows where interactable will be placed
+        cellIndicator.SetActive(true);
 
         // Places down interactable
         inputManager.OnClicked += PlaceStructure;
@@ -103,9 +109,6 @@ public class PlacementSystem : MonoBehaviour
 
         // Accesses item from list
         selectedData.AddItemAt(gridPosition, database.interactableData[selectedInteractableIndex].Size, database.interactableData[selectedInteractableIndex].ID, placedGameObject.Count - 1);
-
-        // If an item is already in the selected position, cannot place item
-        preview.UpdatePosition(grid.CellToWorld(gridPosition), false);
     }
 
     private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedInteractableIndex)
@@ -126,17 +129,14 @@ public class PlacementSystem : MonoBehaviour
         // Hides grid visual
         gridVisual.SetActive(false);
 
-        // Removes preview from grid
-        preview.StopPreview();
+        // Hides indicator
+        cellIndicator.SetActive(false);
 
         // Unassigns PlaceStructure to OnClicked
         inputManager.OnClicked -= PlaceStructure;
 
         // Unassigns StopPlacement to OnExit
         inputManager.OnExit -= StopPlacement;
-
-        // Assigns preview position to zero
-        lastDetectedPosition = Vector3Int.zero;
     }
 
     private void Update()
@@ -151,19 +151,16 @@ public class PlacementSystem : MonoBehaviour
         // Converts mouse position to the grid
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
 
-        // if lastDetectedPosition is not equal to gridPosition, select cell
-        if (lastDetectedPosition != gridPosition)
-        {
-            // Checks validity of placement
-            bool placementValidity = CheckPlacementValidity(gridPosition, selectedInteractableIndex);
+        // Checks validity of placement
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedInteractableIndex);
 
-            // Updates preview position to the grid
-            preview.UpdatePosition(grid.CellToWorld(gridPosition), placementValidity);
+        // If placementValidity is false, make indicator red
+        previewRenderer.material.color = placementValidity ? Color.lightGreen : Color.red;
 
-            // Transforms the position of mouseIndicator
-            mouseIndicator.transform.position = mousePosition;
+        // Transforms the position of mouseIndicator
+        mouseIndicator.transform.position = mousePosition;
 
-            lastDetectedPosition = gridPosition;
-        }
+        // Converts grid position back to world position
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 }
